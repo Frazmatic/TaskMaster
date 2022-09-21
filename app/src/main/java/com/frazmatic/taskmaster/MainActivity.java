6c8cc5325d437f62ec99efb479491e09f25585c7
Joshua Frazer <joshua.frazer.work@gmail.com>
2022-09-20 17:50:31 -0700

    completed settings & task detail pages
    Now uses getDefaultSharedPreferences() to save settings information. Passes information from Main to Details with Intents

--- app/src/main/java/com/frazmatic/taskmaster/MainActivity.java
+++ app/src/main/java/com/frazmatic/taskmaster/MainActivity.java
@@ -3,15 +3,38 @@ package com.frazmatic.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences settings;
+    private TextView mainTitle;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        updateUserName();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUserName();
+    }
+
+    private void updateUserName(){
+        username = settings.getString(Settings.USERNAME_KEY, "No User Name");
+        mainTitle = (TextView)findViewById(R.id.textMainTitle);
+        mainTitle.setText(username + "'s Tasks:");
     }
 
     public void addTask(View view){
@@ -21,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void allTasks(View view){
         Intent intent = new Intent(this, AllTasks.class);
+        startActivity(intent);
+    }
+
+    public void taskDetails(View view){
+        Intent intent = new Intent(this, TaskDetail.class);
+        String buttonText = ((Button)view).getText().toString();
+        intent.putExtra("taskTitle", buttonText);
+        startActivity(intent);
+    }
+
+    public void settings(View view){
+        Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/frazmatic/taskmaster/Settings.java
@@ -0,0 +1,35 @@
+package com.frazmatic.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Settings extends AppCompatActivity {
+    private SharedPreferences settings;
+    public static String USERNAME_KEY = "userName";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = settings.getString(Settings.USERNAME_KEY, "");
+        if (!username.isEmpty()){
+            EditText usernameText = ((EditText)findViewById(R.id.editTextUserName));
+            usernameText.setText(username);
+        }
+    }
+
+    public void saveSettings(View view){
+       SharedPreferences.Editor editor = settings.edit();
+       String username = ((EditText)findViewById(R.id.editTextUserName)).getText().toString();
+       editor.putString(USERNAME_KEY, username);
+       editor.apply();
+       Toast.makeText(Settings.this, "Saving Settings", Toast.LENGTH_SHORT).show();
+    }
+}
